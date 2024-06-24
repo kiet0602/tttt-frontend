ddec90e693bfab0b58494f0417127e6e3e928a75
kiet/0602 <haidangteen3@gmail.com>
2024-06-24 18:59:00 +0700

    xong

--- src/page/Profile/Profile.jsx
+++ src/page/Profile/Profile.jsx
@@ -228,7 +228,7 @@ const Profile = () => {
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-lg">
           <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5" id="exampleModalLabel">
